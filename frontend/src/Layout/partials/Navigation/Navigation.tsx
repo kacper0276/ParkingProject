8190f70e030f761d1421d7875fa9c514c1432dd3
kacper0276 <kacper0276@op.pl>
2024-06-19 19:57:38 +0200

    [FE] Update navigations elements

--- frontend/src/Layout/partials/Navigation/Navigation.tsx
+++ frontend/src/Layout/partials/Navigation/Navigation.tsx
@@ -1,15 +1,32 @@
 import { useRef } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./Navigation.module.css";
 
 export default function Navigation() {
   const buttonMobile = useRef<HTMLDivElement>(null);
   const navigationList = useRef<HTMLUListElement>(null);
+  const navigate = useNavigate();
 
   const showMenu = () => {
     if (buttonMobile.current && navigationList.current) {
       buttonMobile.current.classList.toggle(`${styles.active}`);
       navigationList.current.classList.toggle(`${styles.active}`);
+    }
+  };
+
+  const handleClick = () => {
+    const userInput = prompt("Zczytaj numer biletu");
+
+    if (userInput !== null) {
+      navigate(`/platnosc/${userInput}`);
+    }
+  };
+
+  const handleClickToOpenBarrier = () => {
+    const userInput = prompt("Zczytaj numer biletu");
+
+    if (userInput !== null) {
+      navigate(`/otworz-szlaban/${userInput}`);
     }
   };
 
@@ -27,13 +44,26 @@ export default function Navigation() {
       <ul className={`${styles.navigation_list}`} ref={navigationList}>
         <li className={`${styles.navigation_element}`}>
           <Link to="/" className={`${styles.navigation_link}`}>
-            Strona główna
+            Pobierz bilet
           </Link>
         </li>
         <li className={`${styles.navigation_element}`}>
           <Link to={"/panel-admina"} className={`${styles.navigation_link}`}>
             Panel administratora
           </Link>
+        </li>
+        <li className={`${styles.navigation_element}`}>
+          <button className={`${styles.navigation_link}`} onClick={handleClick}>
+            Płatność za bilet
+          </button>
+        </li>
+        <li className={`${styles.navigation_element}`}>
+          <button
+            className={`${styles.navigation_link}`}
+            onClick={handleClickToOpenBarrier}
+          >
+            Otwórz szlaban
+          </button>
         </li>
       </ul>
     </nav>
