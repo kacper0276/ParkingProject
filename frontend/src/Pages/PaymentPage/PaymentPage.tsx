7b6de4f5053565e759f967e9e8b1fe17a8f7f20e
kacper0276 <kacper0276@op.pl>
2024-06-19 19:56:34 +0200

    [FE] Implements method to Pay for parking

--- frontend/src/Pages/PaymentPage/PaymentPage.tsx
+++ frontend/src/Pages/PaymentPage/PaymentPage.tsx
@@ -1,8 +1,40 @@
 import { useParams } from "react-router-dom";
 import styles from "./PaymentPage.module.css";
+import axios from "axios";
+import { API_URL } from "../../App";
+import { useEffect, useState } from "react";
+
+interface ICost {
+  totalCost: string;
+}
 
 export default function PaymentPage() {
   const { id } = useParams();
+  const [cost, setCost] = useState<ICost>({ totalCost: "0" });
 
-  return <div className={`${styles.main_container}`}>Płatność: {id}</div>;
+  const fetchCostForTicket = async () => {
+    axios.get(`${API_URL}/calculate-cost/${id}`).then((res) => {
+      setCost(res.data);
+    });
+  };
+
+  const payForTicket = async () => {
+    axios.get(`${API_URL}/pay-for-ticket/${id}`).then((res) => {
+      alert(res.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchCostForTicket();
+  }, []);
+
+  return (
+    <div className={`${styles.main_container}`}>
+      <p>Płatność biletu o numerze: {id}</p>
+      <p>Kwota to: {cost.totalCost}</p>
+      <button className={`${styles.paid_button}`} onClick={payForTicket}>
+        Opłać bilet
+      </button>
+    </div>
+  );
 }
